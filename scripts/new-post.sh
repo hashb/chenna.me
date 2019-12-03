7d43061a291f494f38bae9bb0fddaf0e4d67bee4
Chenna Kautilya <chenna@outlook.com>
2019-12-03 11:23:48 -0800

    :squirrel: Added excerpt in new-post.sh

--- scripts/new-post.sh
+++ scripts/new-post.sh
@@ -13,6 +13,10 @@ echo "date: $(date '+%Y-%m-%d %T %z')" | tee -a $file_name
 echo "last_modified_at: $(date '+%Y-%m-%d %T %z')" | tee -a $file_name
 echo "tags: [Captain's log]" | tee -a $file_name
 echo "---" | tee -a $file_name
+echo "" | tee -a $file_name
+echo "This week in review:" | tee -a $file_name
+echo "" | tee -a $file_name
+echo "<!-- more -->" | tee -a $file_name
 
 echo $file_name
 
