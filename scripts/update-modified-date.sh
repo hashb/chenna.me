05ed23839be9926994eb5025c9a55695515f4db2
Chenna Kautilya <chenna@outlook.com>
2020-12-04 00:26:13 -0600

    fix pre-commit hook

--- scripts/update-modified-date.sh
+++ scripts/update-modified-date.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+
 # https://blog.nerde.pw/2016/08/09/jekyll-last-modified-date.html
 echo ""
 echo "************************************"
@@ -7,7 +12,11 @@ echo "*   updating last_modified_at...   *"
 echo "************************************"
 git diff --cached --name-status | while read a b; do
   echo "* Processing $b..."
-  sed -i "/---.*/,/---.*/s/^last_modified_at:.*$/last_modified_at: $(date -u "+%Y-%m-%d %T %z")/" "$b"
+  if exists gsed; then
+    gsed -i "/---.*/,/---.*/s/^last_modified_at:.*$/last_modified_at: $(date -u "+%Y-%m-%d %T %z")/" "$b"
+  else
+    sed -i "/---.*/,/---.*/s/^last_modified_at:.*$/last_modified_at: $(date -u "+%Y-%m-%d %T %z")/" "$b"
+  fi
   git add $b
 done
 echo ""
